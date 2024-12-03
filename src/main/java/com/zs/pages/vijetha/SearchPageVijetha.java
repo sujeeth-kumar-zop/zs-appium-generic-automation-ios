c26988060f5a16f6bc94d9e63609aa2aff4b720b
NagesharaoShridharKunthe <nagesharao.kunte@zopsmart.com>
2024-12-03 17:00:52 +0530

    feature/searchFunctionality | Nagesharao | updated SearchPageVijetha.java file to remove common functions between Tamimi and Vijetha and only inculde functions that are specific to Vijetha

--- src/main/java/com/zs/pages/vijetha/SearchPageVijetha.java
+++ src/main/java/com/zs/pages/vijetha/SearchPageVijetha.java
@@ -4,26 +4,10 @@ import com.zs.locators.VijethaLocators;
 import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 public class SearchPageVijetha {
     private static IOSDriver driver;
-    private final WebDriverWait wait;
-
-    public SearchPageVijetha(IOSDriver CurrentDriver, WebDriverWait wait){
+    public SearchPageVijetha(IOSDriver CurrentDriver){
         driver = CurrentDriver;
-        this.wait=wait;
-    }
-    public void enterProduct(String Product){
-        By SearchLoc = VijethaLocators.getSearchPageLocator("searchBar");
-        WebElement searchBox = driver.findElement(SearchLoc);
-        wait.until(ExpectedConditions.visibilityOf(searchBox)).sendKeys(Product);
-    }
-    public void clickCancelButton(){
-        By CancelButtonLoc = VijethaLocators.getSearchPageLocator("cancel");
-        WebElement CancelBtn = driver.findElement(CancelButtonLoc);
-        CancelBtn.click();
     }
     public void clickBackButton(){
         By BackButtonLoc = VijethaLocators.getSearchPageLocator("back");
@@ -31,15 +15,4 @@ public class SearchPageVijetha {
         BackButtonBtn.click();
     }
 
-    public boolean verifyProduct(){
-        By SpecificProductLoc = VijethaLocators.getProductPageLocators("SpecificProduct");
-        WebElement specificProduct = driver.findElement(SpecificProductLoc);
-        String specificProductName = specificProduct.getText();
-        return specificProductName.contains("Bingo Hashtags Spicy Masala");
-    }
-    public void productSearchFlow(){
-        SearchPageVijetha searchPageVijetha =new SearchPageVijetha(driver, wait);
-        searchPageVijetha.clickCancelButton();
-        searchPageVijetha.enterProduct("Bingo Hashtags Spicy Masala");
-    }
 }
