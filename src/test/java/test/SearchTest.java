3f5964423989c3e4fb1fcb3ce3a2011a7a8f4207
NagesharaoShridharKunthe <nagesharao.kunte@zopsmart.com>
2024-12-03 15:27:52 +0530

    feature/searchFunctionality | Nagesharao | updated SearchTest.java file to implement generic search functionality

--- src/test/java/test/SearchTest.java
+++ src/test/java/test/SearchTest.java
@@ -1,7 +1,6 @@
 package test;
 
-import com.zs.pages.tamimi.SearchPageTamimi;
-import com.zs.pages.vijetha.SearchPageVijetha;
+import com.zs.pages.common.SearchPage;
 import config.BaseTest;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
@@ -16,16 +15,9 @@ public class SearchTest extends BaseTest{
     @Parameters("appName")
     public void addToCart(@Optional String appName) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        if(appName.equals("Tamimi")){
-            SearchPageTamimi searchPageTamimi = new SearchPageTamimi(driver, wait);
-            searchPageTamimi.generalSearchFlow();
-            assertTrue(searchPageTamimi.verifyGeneralProduct());
-        }
-        else{
-            SearchPageVijetha searchPageVijetha = new SearchPageVijetha(driver, wait);
-            searchPageVijetha.generalSearchFlow();
-            assertTrue(searchPageVijetha.verifyGeneralProduct());
-        }
+        SearchPage searchPage = new SearchPage(driver, wait);
+        searchPage.generalSearchFlow(appName);
+        assertTrue(searchPage.verifyGeneralProduct(appName));
     }
 
 }
