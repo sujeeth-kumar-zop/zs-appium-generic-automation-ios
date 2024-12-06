320d386cd470bf6c33a0667bcfedb39a72d13a94
NagesharaoShridharKunthe <nagesharao.kunte@zopsmart.com>
2024-12-06 16:14:24 +0530

    feature/searchFunctionality | Nagesharao | Updated SearchTest.java file to add Loggers

--- src/test/java/test/SearchTest.java
+++ src/test/java/test/SearchTest.java
@@ -2,6 +2,8 @@ package test;
 
 import com.zs.pages.common.SearchPage;
 import config.BaseTest;
+import com.zs.utils.ExtentReport;
+import com.zs.utils.LoggerUtil;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import java.time.Duration;
@@ -13,7 +15,8 @@ public class SearchTest extends BaseTest{
 
     @Test
     @Parameters("appName")
-    public void addToCart(@Optional String appName) {
+    public void generalSearch(@Optional String appName) {
+        LoggerUtil.setExtentTest(ExtentReport.getTest());
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         SearchPage searchPage = new SearchPage(driver, wait);
         searchPage.generalSearchFlow(appName);
