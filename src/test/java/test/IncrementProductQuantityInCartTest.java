e5abd8a7a3d0d55d6806b7cf30d67ca932e11624
NagesharaoShridharKunthe <nagesharao.kunte@zopsmart.com>
2024-12-06 16:12:02 +0530

    feature/searchFunctionality | Nagesharao | Included IncrementProductQuantityInCartTest.java file

---
+++ src/test/java/test/IncrementProductQuantityInCartTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import com.zs.pages.common.CartPage;
+import config.BaseTest;
+import com.zs.utils.ExtentReport;
+import com.zs.utils.LoggerUtil;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class IncrementProductQuantityInCartTest extends BaseTest {
+    @Test
+    @Parameters("appName")
+    public void incrementProductQuantity(@Optional String appName) throws InterruptedException {
+        LoggerUtil.setExtentTest(ExtentReport.getTest());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        CartPage cartPage = new CartPage(driver, wait);
+        cartPage.incrementProductFlow(appName);
+        assertTrue(cartPage.verifyIncrement(appName));
+    }
+}
